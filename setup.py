c967bd27463c61a860c6d0de6dd924e3e501f94c
jcarron <j.carron@sussex.ac.uk>
2019-08-07 10:01:11 +0200

    clean

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 def configuration(parent_package='', top_path=''):
     config = Configuration('', parent_package, top_path)
-    config.add_extension('lenspyx.bicubic', ['lenspyx/bicubic.f90'])
+    config.add_extension('lenspyx.bicubic.bicubic', ['lenspyx/bicubic/bicubic.f90'])
     config.add_extension('lenspyx.shts.fsht', ['lenspyx/shts/shts.f90'],
                          extra_link_args=['-lgomp'],
                          libraries=['gomp'], extra_f90_compile_args=['-fopenmp', '-w'])
