2ff279be5127f2109c972da974888ec66d5c8fd0
jcarron <j.carron@sussex.ac.uk>
2019-08-07 16:50:07 +0200

    pyfftw

--- setup.py
+++ setup.py
@@ -10,19 +10,18 @@ def configuration(parent_package='', top_path=''):
     config = Configuration('', parent_package, top_path)
     config.add_extension('lenspyx.bicubic.bicubic', ['lenspyx/bicubic/bicubic.f90'])
     config.add_extension('lenspyx.shts.fsht', ['lenspyx/shts/shts.f90'],
-                         extra_link_args=['-lgomp'],
-                         libraries=['gomp'], extra_f90_compile_args=['-fopenmp', '-w'])
+                extra_link_args=['-lgomp'],libraries=['gomp'], extra_f90_compile_args=['-fopenmp', '-w'])
     return config
 
 setup(
     name='lenspyx',
     version='0.0.1',
-    packages=['lenspyx', 'tests'],
+    packages=['lenspyx', 'tests', 'lenspyx.bicubic'],
     url='https://github.com/carronj/lenspyx',
     author='Julien Carron',
     author_email='to.jcarron@gmail.com',
     description='lensed CMB sims pipe',
-    install_requires=['healpy', 'numpy'],
+    install_requires=['healpy', 'numpy', 'pyfftw'],
     long_description=long_description,
     configuration=configuration)
 
