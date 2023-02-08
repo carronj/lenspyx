4e559992fcfc678d82c254ff3b0c7377cf3e7535
Julien Carron <to.jcarron@gmail.com>
2023-02-08 19:23:38 +0100

    script

--- lenspyx/tests/test_lmaxgl.py
+++ lenspyx/tests/test_lmaxgl.py
@@ -8,21 +8,27 @@ import healpy as hp, numpy as np
 import pylab as pl
 from time import time
 
-def get_lensedcmb(ffi:duccd, dlmax_gl):
-    lmax_len, mmax_len, dlmax, dlmax_gl = 4096, 4096, 1024, dlmax_gl
+def get_lensedcmb(ffi:duccd, ebunl=None, single_precision=False):
+    lmax_len, mmax_len, dlmax = 4096, 4096, 1024
     lmax_unl = lmax_len + dlmax
     mmax_unl = lmax_unl
-    eblm = np.array([hp.synalm(cls_unl['ee'][:lmax_unl + 1]),
+    if ebunl is None:
+        ebunl = np.array([hp.synalm(cls_unl['ee'][:lmax_unl + 1]),
                      hp.synalm(cls_unl['bb'][:lmax_unl + 1])])
-    return ffi.lensgclm(eblm, mmax_unl, 2, lmax_len, mmax_len, False)
+    if single_precision:
+        ebunl = ebunl.astype(np.complex64)
+    else:
+        ebunl = ebunl.astype(np.complex128)
+    return ffi.lensgclm(ebunl, mmax_unl, 2, lmax_len, mmax_len, False), ebunl
+
 def binit(cl, d=10):
     ret = cl.copy()
     for l in range(d, ret.size -d):
         ret[l] = np.mean(cl[l-d:l+d+1])
     return ret
 
-def get_ffi(dlmax_gl, nthreads=4):
-    lmax_len, mmax_len, dlmax, dlmax_gl = 4096, 4096, 1024, dlmax_gl
+def get_ffi(dlmax_gl, nthreads=4, dlmax=1024):
+    lmax_len, mmax_len, dlmax, dlmax_gl = 4096, 4096, dlmax, dlmax_gl
     ffi_ducc, ref_geom = syn_ffi_ducc(lmax_len=lmax_len, dlmax=dlmax,dlmax_gl=dlmax_gl,
                                       nthreads=nthreads)
     return ffi_ducc
@@ -31,7 +37,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='test FFP10-like building')
     parser.add_argument('-nt', dest='nt', default=4, type=int, help='numbers of openMP threads')
-    parser.add_argument('-dlmax_gl', dest='dlmax_gl', default=4, type=int, help='numbers of openMP threads')
+    parser.add_argument('-dlmax_gl', dest='dlmax_gl', default=1024, type=int, help='numbers of openMP threads')
 
     args = parser.parse_args()
     nthreads = args.nt
