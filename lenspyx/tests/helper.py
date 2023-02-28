21cefb01a0cb128f2fc6a6a4df3ad2e2613f2f14
jcarron <to.jcarron@gmail.com>
2023-02-28 22:23:05 +0100

    minors

--- lenspyx/tests/helper.py
+++ lenspyx/tests/helper.py
@@ -1,10 +1,9 @@
-import healpy as hp
-import numpy as np
 import os
-from plancklens.utils import camb_clfile
-from lenscarf import utils_scarf
-from lenscarf import cachers
+import numpy as np
 import lenspyx
+from lenspyx.utils import camb_clfile
+from lenspyx import cachers
+from lenspyx.utils_hp import synalm, almxfl
 from lenspyx.remapping.deflection import deflection as duccd
 from lenspyx.remapping.deflection_029 import deflection as duccd29
 from lenscarf.remapping import deflection as scarfd
@@ -20,8 +19,8 @@ def syn_ffi_ducc(lmax_len = 4096, dlmax=1024, epsilon=1e-5, dlm_fac=1., nthreads
     lmax_unl = lmax_len + dlmax
     lmax_dlm, mmax_dlm = lmax_unl, lmax_unl
     lmaxthingauss = lmax_unl + dlmax_gl
-    plm = hp.synalm(cls_unl['pp'][:lmax_dlm + 1], new=True)
-    dlm = hp.almxfl(plm, dlm_fac * np.sqrt(np.arange(lmax_dlm + 1, dtype=float) * np.arange(1, lmax_dlm + 2)))
+    plm = synalm(cls_unl['pp'][:lmax_dlm + 1], lmax_dlm, mmax_dlm)
+    dlm = almxfl(plm, dlm_fac * np.sqrt(np.arange(lmax_dlm + 1, dtype=float) * np.arange(1, lmax_dlm + 2)), mmax_dlm, False)
     ref_geom = utils_geom.Geom.get_thingauss_geometry(lmaxthingauss, 2)
     ffi_ducc = duccd(ref_geom, dlm, mmax_dlm, numthreads=nthreads, verbosity=verbosity, dclm=None, epsilon=epsilon, cacher=cachers.cacher_mem(safe=False))
     return ffi_ducc, ref_geom
@@ -33,8 +32,8 @@ def syn_ffi_ducc_29(lmax_len = 4096, dlmax=1024, epsilon=1e-5, dlm_fac=1., nthre
     lmax_unl = lmax_len + dlmax
     lmax_dlm, mmax_dlm = lmax_unl, lmax_unl
     lmaxthingauss = lmax_unl + dlmax_gl
-    plm = hp.synalm(cls_unl['pp'][:lmax_dlm + 1], new=True)
-    dlm = hp.almxfl(plm, dlm_fac * np.sqrt(np.arange(lmax_dlm + 1, dtype=float) * np.arange(1, lmax_dlm + 2)))
+    plm = synalm(cls_unl['pp'][:lmax_dlm + 1], lmax_dlm, mmax_dlm)
+    dlm = almxfl(plm, dlm_fac * np.sqrt(np.arange(lmax_dlm + 1, dtype=float) * np.arange(1, lmax_dlm + 2)), mmax_dlm, False)
     ref_geom = utils_geom.Geom.get_thingauss_geometry(lmaxthingauss, 2)
     ffi_ducc = duccd29(ref_geom, dlm, mmax_dlm, numthreads=nthreads, verbosity=verbosity, dclm=None, epsilon=epsilon, cacher=cachers.cacher_mem(safe=False))
     return ffi_ducc, ref_geom
@@ -47,11 +46,10 @@ def syn_ffi_lenscarf(lmax_len = 4096, dlmax=1024, target_res=1.7, dlm_fac=1., nt
     lmax_dlm, mmax_dlm = lmax_unl, lmax_unl
     lmaxthingauss = lmax_unl + dlmax_gl
 
-
-    plm = hp.synalm(cls_unl['pp'][:lmax_dlm + 1], new=True)
-    dlm = hp.almxfl(plm, dlm_fac * np.sqrt(np.arange(lmax_dlm + 1) * np.arange(1, lmax_dlm + 2)))
-    ref_geom = utils_scarf.Geom.get_thingauss_geometry(lmaxthingauss, 2)
-    pbref_geom = utils_scarf.pbdGeometry(ref_geom, utils_scarf.pbounds(0., 2 * np.pi))
+    plm = synalm(cls_unl['pp'][:lmax_dlm + 1], lmax_dlm, mmax_dlm)
+    dlm = almxfl(plm, dlm_fac * np.sqrt(np.arange(lmax_dlm + 1, dtype=float) * np.arange(1, lmax_dlm + 2)), mmax_dlm, False)
+    ref_geom = utils_geom.Geom.get_thingauss_geometry(lmaxthingauss, 2)
+    pbref_geom = utils_geom.pbdGeometry(ref_geom, utils_geom.pbounds(0., 2 * np.pi))
     fft_tr, sht_tr = nthreads, nthreads
     ffi = scarfd(pbref_geom, target_res, dlm, mmax_dlm, fft_tr, sht_tr, verbose=verbose, dclm=None, cacher=cachers.cacher_mem(safe=False))
     return ffi, ref_geom
