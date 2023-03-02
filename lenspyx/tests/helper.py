f12587841d5cbdbbc04c759b46a63f0f85d9f5ae
jcarron <to.jcarron@gmail.com>
2023-03-02 22:59:54 +0100

    minor

--- lenspyx/tests/helper.py
+++ lenspyx/tests/helper.py
@@ -6,7 +6,6 @@ from lenspyx import cachers
 from lenspyx.utils_hp import synalm, almxfl
 from lenspyx.remapping.deflection import deflection as duccd
 from lenspyx.remapping.deflection_029 import deflection as duccd29
-from lenscarf.remapping import deflection as scarfd
 from lenspyx.remapping import utils_geom
 path2cls = os.path.dirname(lenspyx.__file__)
 cls_unl  = camb_clfile(path2cls + '/data/cls/FFP10_wdipole_lenspotentialCls.dat')
@@ -42,6 +41,7 @@ def syn_ffi_lenscarf(lmax_len = 4096, dlmax=1024, target_res=1.7, dlm_fac=1., nt
     """"Returns realistic LCDM deflection field scaled by dlm_fac
 
     """
+    from lenscarf.remapping import deflection as scarfd
     lmax_unl = lmax_len + dlmax
     lmax_dlm, mmax_dlm = lmax_unl, lmax_unl
     lmaxthingauss = lmax_unl + dlmax_gl
