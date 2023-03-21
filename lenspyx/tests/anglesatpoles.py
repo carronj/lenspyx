abe37363b26d98a93407bf08fc3202b12f5e28b9
jcarron <to.jcarron@gmail.com>
2023-03-21 22:33:50 +0100

    minors

--- lenspyx/tests/anglesatpoles.py
+++ lenspyx/tests/anglesatpoles.py
@@ -7,7 +7,7 @@ from lenspyx.tests import helper
 import numpy as np
 import pylab as pl
 
-gl = utils_geom.Geom.get_gl_geometry(50000).restrict(0.0, 1e-3, True, update_ringstart=True)
+gl = utils_geom.Geom.get_cc_geometry(100000, 100).restrict(0.0, 1e-3, True, update_ringstart=True)
 ffi, _ = helper.syn_ffi_ducc_29()
 ffi.geom = gl
 ptg = ffi._get_ptg()
@@ -19,21 +19,24 @@ print(chi.shape, ptg.shape)
 print(ffi.tim)
 nph = int(gl.nph[0])
 phi = 2 * np.pi / nph * np.arange(nph)
-for ir in [0, gl.theta.size-1]:
+for ir in [0, 1, gl.theta.size-2, gl.theta.size-1]:
     pl.figure()
-    print(gl.theta[ir])
     sli = slice(ir * nph, ir * nph + nph)
-    if gl.theta[ir] < np.pi * 0.05:
-        pl.plot(phi, ptg[sli, 1], label='phip')
-        pl.plot(phi, (beta[sli] + phi) %(2 * np.pi), label='b + phi')
-        pl.plot(phi, chi[sli] % (2 * np.pi), label='chi')
-        pl.plot(phi, (beta[sli])% (2 * np.pi) , label='beta')
+    if gl.theta[ir] < (np.pi * 0.5):
+        pl.plot(phi, ptg[sli, 1], label=r'$\phi^{\prime}$')
+        pl.plot(phi, (beta[sli] + phi) %(2 * np.pi), label=r'$\beta + \phi$')
+        pl.plot(phi, chi[sli] % (2 * np.pi), label=r'$\chi$')
+        pl.plot(phi, (beta[sli])% (2 * np.pi) , label=r'$\beta$')
         pl.legend()
+        pl.title(r'NP, $\theta = %.2f$ amin'%(gl.theta[ir] / np.pi * 180 * 60. ))
+        print('tht dev NP in amin', np.max(np.abs(ptg[sli, 0] - np.sqrt(asqd[sli])))/np.pi * 180 * 60)
         pl.show()
-    if gl.theta[ir] > np.pi * 0.05:
-        pl.plot(phi, ptg[sli, 1], label='phip')
-        pl.plot(phi, (np.pi + phi - beta[sli]) %(2 * np.pi), label='phi + pi - beta')
-        pl.plot(phi, chi[sli] % (2 * np.pi), label='chi')
-        pl.plot(phi, (beta[sli]- 1*np.pi)% (2 * np.pi) , label='beta - pi')
+    if gl.theta[ir] > (np.pi * 0.5):
+        pl.plot(phi, ptg[sli, 1], label=r'$\phi^{\prime}$')
+        pl.plot(phi, (np.pi + phi - beta[sli]) %(2 * np.pi), label=r'$\phi + \pi - \beta$')
+        pl.plot(phi, chi[sli] % (2 * np.pi), label=r'$\chi$')
+        pl.plot(phi, (beta[sli]- 1*np.pi)% (2 * np.pi) , label=r'$\beta - \pi$')
+        print('tht dev SP in amin', np.max(np.abs( (np.pi-ptg[sli, 0]) - np.sqrt(asqd[sli])))/np.pi * 180 * 60)
         pl.legend()
+        pl.title(r'SP, $\pi - \theta = %.2f$ amin'%( (np.pi-gl.theta[ir]) / np.pi * 180 * 60. ))
         pl.show()
--- lenspyx/tests/lensaliasing.py
+++ lenspyx/tests/lensaliasing.py
@@ -6,7 +6,6 @@ import ducc0.sht.experimental
 import numpy as np
 from lenspyx.tests.helper import syn_ffi_ducc_29, cls_len, cls_unl
 from lenspyx.utils_hp import synalm, Alm, alm2cl, alm_copy, almxfl
-from lenspyx.utils import timer
 from lenspyx.remapping import utils_geom
 import multiprocessing
 import argparse
@@ -36,7 +35,7 @@ def build_ref_alm(lmax_sky, lmax_wted, spin):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='test aliasing')
-    parser.add_argument('-s', dest='spin', type=int, default=0, help='spin to test')
+    parser.add_argument('-s', dest='spin', type=int, default=2, help='spin to test')
     args = parser.parse_args()
 
     dlmax_gl = 1024
@@ -54,20 +53,20 @@ if __name__ == '__main__':
 
     cls_in = [alm2cl(a, a, lmax_wted, mmax_wted, lmax_wted) for a in alm_ref]
     gls = [utils_geom.Geom.get_thingauss_geometry(lmax_sky + dlmax_gl, args.spin),
-           utils_geom.Geom.get_gl_geometry(lmax_sky + dlmax_gl, True),
+           utils_geom.Geom.get_cc_geometry(lmax_sky + dlmax_gl + 2, 2 * (lmax_sky + dlmax_gl) + 2).thinout(args.spin, True),
            utils_geom.Geom.get_f1_geometry(lmax_sky + dlmax_gl + 2, 2 * (lmax_sky + dlmax_gl) + 2).thinout(args.spin, True),
-           utils_geom.Geom.get_f1_geometry(lmax_sky + dlmax_gl + 2, 2 * (lmax_sky + dlmax_gl) + 2),
            utils_geom.Geom.get_f1_geometry(lmax_sky + dlmax_gl + 2, 2 * (lmax_sky + dlmax_gl) + 2)]
     gls += [utils_geom.Geom.get_healpix_geometry(int(np.sqrt(gls[0].npix()/12)))]
-    gls_lab = ['thGL', 'GL no thin', 'F1 lmax + 2', 'F1 wMRtrick','F1 lmax + 2 (no thinning)', 'HP same #pix as GL'] # 'CC lmax + 2', 'CC wMRtrick' # Cant uses on the poles for now
+    gls_lab = ['thGL', 'thCC', 'thF1', 'F1 2d', 'HP same #pix as GL'] # 'CC lmax + 2', 'CC wMRtrick' # Cant uses on the poles for now
     fig, axes = pl.subplots(3, 1, figsize=(3 * 7, 5))
     ratios = []
+    clres_ref = []
     for igeom, (gl, gl_lab) in enumerate(zip(gls, gls_lab)):
         tim = timer('this geom', False)
         ffi_ducc = duccd29(gl, dlm, mmax_sky, numthreads=nthreads, verbosity=True, dclm=None, epsilon=1e-11)
-        if 'MRtrick' in gl_lab:
+        if '2d' in gl_lab:
             m = ffi_ducc.gclm2lenmap(alm_unl, mmax_sky, args.spin, False)
-            alm_out = ducc0.sht.experimental.analysis_2d(map=np.atleast_2d(m).reshape((1, ntheta, nphi)),
+            alm_out = ducc0.sht.experimental.analysis_2d(map=np.atleast_2d(m).reshape((ncomp, ntheta, nphi)),
                                                 spin=args.spin, geometry=gl_lab[:2], lmax=lmax_wted, nthreads=nthreads)
         else:
             alm_out = ffi_ducc.lensgclm(alm_unl, mmax_sky, args.spin, lmax_wted, mmax_wted)
@@ -86,12 +85,12 @@ if __name__ == '__main__':
             pl.plot(ls, np.sqrt(alm2cl(diff, diff, lmax_wted, mmax_wted, lmax_wted)[ls] / cls_in[i][ls]), label=lab)
             del diff
         pl.sca(axes[2])
-        for i, lab in zip(range(ncomp), [gl_lab + ' G (%.2f Mpix)' % (gl.npix() / 1e6), gl_lab + 'C']):
+        for i, lab in zip(range(ncomp), [gl_lab + ' G (%.2f Mpix)' % (gl.npix() / 1e6), gl_lab + ' C']):
             diff = alm_out[i] - alm_ref[i]
             if igeom == 0:
-                clres_ref = alm2cl(diff, diff, lmax_wted, mmax_wted, lmax_wted)
+                clres_ref.append(alm2cl(diff, diff, lmax_wted, mmax_wted, lmax_wted))
             else:
-                this_ratio = np.sqrt(alm2cl(diff, diff, lmax_wted, mmax_wted, lmax_wted)[ls] / clres_ref[ls])
+                this_ratio = np.sqrt(alm2cl(diff, diff, lmax_wted, mmax_wted, lmax_wted)[ls] / clres_ref[i][ls])
                 pl.plot(ls, this_ratio, label=lab)
                 ratios.append(this_ratio)
             del diff
