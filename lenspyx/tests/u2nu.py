4c10c794eee3ff480b0690fb8607ed628e4eeccd
jcarron <to.jcarron@gmail.com>
2023-03-27 16:16:38 +0200

    minor

--- lenspyx/tests/u2nu.py
+++ lenspyx/tests/u2nu.py
@@ -11,7 +11,9 @@ if __name__ == '__main__':
     parser.add_argument('-dlmaxgl', dest='dlmax_gl', type=int, default=1024, help='buffer to GL grid')
     parser.add_argument('-dlmax', dest='dlmax', type=int, default=1024, help='buffer to lensed alms')
     parser.add_argument('-eps', dest='epsilon', type=float, default=5, help='-log10 of nufft accuracy')
-    parser.add_argument('-n', dest='nt', type=int, default=4, help='number of threads')
+    parser.add_argument('-nmin', dest='ntmin', type=int, default=4, help='min number of threads')
+    parser.add_argument('-nmax', dest='ntmax', type=int, default=4, help='max number of threads')
+
     args = parser.parse_args()
 
     spin = args.spin
@@ -22,9 +24,11 @@ if __name__ == '__main__':
     #ffi_29, _ = syn_ffi_ducc_29(nthreads=nthreads,  lmax_len=lmax_len, dlmax=dlmax, dlmax_gl=dlmax_gl, verbosity=1)
 
     alm = syn_alms(spin, ctyp=np.complex64 if ffi.single_prec else np.complex128)
-    ffi.tim = timer('', False)
-    ffi.gclm2lenmap(alm, None, spin, False)
-    print("u2nu: %s threads, %.3f Mpix / s"%(nthreads, geom.npix() * 1e-6 / ffi.tim.keys['u2nu']))
+    for n in range(args.nmin, args.nmax + 1):
+        ffi.sht_tr = n
+        ffi.tim = timer('', False)
+        ffi.gclm2lenmap(alm, None, spin, False)
+        print("u2nu: %s threads, %.3f Mpix / s"%(nthreads, geom.npix() * 1e-6 / ffi.tim.keys['u2nu']))
 
     #ffi_29.tim = timer('', False)
     #ffi_29.gclm2lenmap(alm, None, spin, False)
