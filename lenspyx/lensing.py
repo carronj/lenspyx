21a8a28334089fa39f2d6e5ce8cb42e5532ddc37
jcarron <to.jcarron@gmail.com>
2023-04-19 22:51:27 +0200

    misc

--- lenspyx/lensing.py
+++ lenspyx/lensing.py
@@ -1,11 +1,12 @@
 from __future__ import print_function, annotations
-import numpy as np
 from os import cpu_count
+import numpy as np
 from lenspyx.remapping.utils_geom import Geom
 from lenspyx.remapping.deflection_029 import deflection
 from lenspyx import cachers
 
-def alm2lenmap(alm, dlms, geom:tuple[str, dict]=('healpix', {'nside':2048}), epsilon=1e-7, verbose=True, nthreads:int=0):
+
+def alm2lenmap(alm, dlms, geometry:tuple[str, dict]=('healpix', {'nside':2048}), epsilon=1e-7, verbose=True, nthreads:int=0):
     r"""Computes a deflected spin-0 healpix map from its alm and deflection field alm.
 
         Args:
@@ -16,22 +17,19 @@ def alm2lenmap(alm, dlms, geom:tuple[str, dict]=('healpix', {'nside':2048}), eps
                     (e.g. :math:`\sqrt{L(L+1)}\phi_{LM}` with :math:`\phi` the lensing potential)
                     The curl can be set to None if irrelevant.
 
-            facres(optional): the deflected map is constructed by interpolation of the undeflected map,
-                              built at target res. ~ :math:`0.7 * 2^{-\rm facres}.` arcmin
-            nband(optional): To avoid dealing with too many large maps in memory, the operations is split in bands.
+            epsilon(optional): target accuracy of the result (defaults to 1e-7)
             verbose(optional): If set, prints a bunch of timing and other info. Defaults to true.
-            experimental(optional): well, that's experimental
             nthreads(optional): number of threads to use (defaults to os.cpu_count())
 
         Returns:
-            Deflected healpy map at resolution nside.
+            lensed map, array of size given by the number of pixels of input geometry
 
 
     """
-    assert len(dlms) == 2
+    assert len(dlms) == 2, len(dlms)
     if nthreads <= 0:
         nthreads = cpu_count()
-    geo = getattr(Geom, '_'.join(['get', geom[0], 'geometry']))(**(geom[1]))
+    geo = getattr(Geom, '_'.join(['get', geometry[0], 'geometry']))(**(geometry[1]))
     defl = deflection(geo, dlms[0], None, dclm=dlms[1], epsilon=epsilon, numthreads=nthreads, verbosity=0,
                       cacher=cachers.cacher_mem(safe=False))
     ret = defl.gclm2lenmap(alm, None, 0, False)
@@ -39,7 +37,8 @@ def alm2lenmap(alm, dlms, geom:tuple[str, dict]=('healpix', {'nside':2048}), eps
         print(defl.tim)
     return ret
 
-def alm2lenmap_spin(gclm, dlms, spin:int, epsilon:float=1e-7, verbose=True, nthreads:int=0):
+
+def alm2lenmap_spin(gclm, dlms, spin:int, geometry:tuple[str, dict]=('healpix', {'nside':2048}), epsilon:float=1e-7, verbose=True, nthreads:int=0):
     r"""Computes a deflected spin-weight Healpix map from its gradient and curl modes and deflection field alm.
 
         Args:
@@ -60,20 +59,20 @@ def alm2lenmap_spin(gclm, dlms, spin:int, epsilon:float=1e-7, verbose=True, nthr
 
 
         Returns:
-            Deflected healpy maps at resolution nside (real and imaginary parts).
+            lensed maps for input geometry (real and imaginary parts
+            array of size given by the number of pixels of input geometry
 
 
     """
     if spin == 0:
-        return alm2lenmap(gclm, dlms, nside, epsilon=epsilon, nband=nband, facres=facres, verbose=verbose,
-                          experimental=experimental, nthreads=nthreads)
-    assert len(gclm) == 2
-    assert len(dlms) == 2
+        return alm2lenmap(gclm, dlms, geometry=geometry, epsilon=epsilon, verbose=verbose, nthreads=nthreads)
+    assert len(gclm) == 2, len(gclm)
+    assert len(dlms) == 2, len(dlms)
     if nthreads <= 0:
         nthreads = cpu_count()
 
-    geom = Geom.get_healpix_geometry(nside)
-    defl = deflection(geom, dlms[0], None, dclm=dlms[1], epsilon=epsilon, numthreads=nthreads, verbosity=0,
+    geo = getattr(Geom, '_'.join(['get', geometry[0], 'geometry']))(**(geometry[1]))
+    defl = deflection(geo, dlms[0], None, dclm=dlms[1], epsilon=epsilon, numthreads=nthreads, verbosity=0,
                       cacher=cachers.cacher_mem(safe=False))
     if gclm[1] is None:
         gclm[1] = np.zeros_like(gclm[0])
--- lenspyx/tests/lensing.py
+++ lenspyx/tests/lensing.py
@@ -1,11 +1,19 @@
 from lenspyx.tests.helper import syn_alms, syn_dlm
 from lenspyx import alm2lenmap, alm2lenmap_spin
 
-spin = 0
 lmax = 500
 alm = syn_alms(0, lmax)
+eblm = syn_alms(2, lmax)
 dlm = syn_dlm(lmax)
 
 # default behavior
-alm2lenmap(alm, [dlm, None])
+T = alm2lenmap(alm, [dlm, None])
+Q, U = alm2lenmap_spin(eblm, [dlm, None], 2)
+
+# thinned GL:
+T = alm2lenmap(alm, [dlm, None], geometry=('thingauss', {'smax': 2, 'lmax':lmax}))
+Q, U = alm2lenmap_spin(eblm, [dlm, None], 2, geometry=('thingauss', {'smax': 2, 'lmax':lmax}))
+
+
+
 
