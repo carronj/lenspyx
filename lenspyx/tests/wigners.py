5b2b201e57a0eb27ad0d3b480ff8768b2f7b88d7
jcarron <to.jcarron@gmail.com>
2023-04-23 22:35:48 +0200

    wignerpos

---
+++ lenspyx/tests/wigners.py
@@ -0,0 +1,41 @@
+try:
+    from plancklens.wigners import wigners as wigners_pl
+except:
+    assert 0, 'cant do this test witout plancklens'
+import numpy as np
+import time
+from ducc0.misc import GL_thetas, GL_weights
+from lenspyx.utils_hp import gauss_beam
+from lenspyx.wigners import wigners
+
+print('All these numbers must be small')
+lmaxs = [500, 1000, 2000, 4000, 5000]
+for lmax in lmaxs:
+    npts = (3 * lmax) // 2 + 1
+    ls = np.arange(lmax + 1)
+    tht = GL_thetas(npts)[::-1]
+    wg = GL_weights(npts, 1)[::-1] / (2 * np.pi)
+    xg = np.cos(tht)
+    cl_in = gauss_beam(1./180 / 60 * np.pi,lmax=lmax)
+    maxdevs = []
+    for s1 in range(-3, 4):
+        for s2 in range(-3, 4):
+            xi12 = wigners.wignerpos(cl_in, tht, s1, s2)
+            clv2 = wigners_pl.wignercoeff(xi12 * wg, xg, s1, s2, lmax)
+            t_ls = ls[max(abs(s1), abs(s2)):]
+            maxdevs.append(np.max(np.abs(clv2[t_ls] / cl_in[t_ls] - 1.)))
+    print('lmax %s %.3e'%(lmax, np.max(maxdevs)))
+    assert np.max(maxdevs) < 1e-9, np.max(maxdevs)
+    if lmax == lmaxs[-1]:
+        for s1s2 in [(0, 0), (0, 2), (2, 0), (0, -2), (-2, 0), (-2, -2)]:
+            s1, s2 = s1s2
+            print('Timings s1 s2  %s %s'%(s1, s2))
+            t0 = time.time()
+            xi12 = wigners.wignerpos(cl_in, tht, s1, s2)
+            t1 = time.time()
+            print('DUCC       %.1e ms' % ((t1 - t0) * 1e3))
+            t0 = time.time()
+            xi12 = wigners_pl.wignerpos(cl_in, xg, s1, s2)
+            t1 = time.time()
+            print('Plancklens %.1e ms' % ((t1 - t0) * 1e3))
+
---
+++ lenspyx/wigners/wigners.py
@@ -0,0 +1,46 @@
+"""This module contains DUCC-based fonctions related to Wigner-small d tranforms
+
+
+"""
+from __future__ import annotations
+import numpy as np
+from ducc0.sht.experimental import alm2leg
+
+
+def wignerpos(cl: np.ndarray, theta: np.ndarray, s1: int, s2: int):
+    r"""Produces Wigner small-d transform defined by
+
+        :math:`\sum_\ell \frac{2\ell + 1}{4\pi} c_\ell d^\ell_{s_1 s_2}(\theta)`
+
+        Args:
+            cl: spectrum of Wigner small-d transform
+            theta: co-latitude in radians (in [0, pi])
+            s1: first spin
+            s2: second spin
+
+        Returns:
+            real array of same size than theta
+
+
+    """
+    if s1 < 0 or (s1 == 0 and s2 > 0):
+        # second cond. branching resulting in spin-0 eval. The case 0 -2 is still not optimal
+        t_cl = cl if (s1 + s2) % 2 == 0 else -cl
+        if s2 < 0:
+            return wignerpos(t_cl, theta, -s1, -s2)
+        else:
+            return wignerpos(t_cl, theta, s2, s1)
+    else:
+        mval = np.array([abs(s1)], dtype=int)
+        lmax = len(cl) - 1
+        sgn = (-1) ** (1 + (s2 if s2 < 0 else 0) + (s2 == 0))
+        gl = sgn * cl * np.sqrt(np.arange(1, 2 * lmax + 3, 2)) / np.sqrt(4 * np.pi)
+        glm_r = gl.astype(np.complex128)
+        if s2 != 0:
+            glm_i = (1j * np.sign(s2)) * gl
+            glm = np.stack([glm_r, glm_i])
+        else:
+            glm = np.atleast_2d(glm_r)
+        mstart = np.array([0], dtype=int)
+        leg = alm2leg(alm=glm, spin=abs(s2), lmax=lmax, mval=mval, mstart=mstart, theta=theta)
+        return leg.squeeze()[0].real if s2 != 0 else leg.squeeze().real
