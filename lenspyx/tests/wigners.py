c42203eeafc83a9ba30c7b0f92cbc5ec4e048b28
jcarron <to.jcarron@gmail.com>
2023-04-24 17:49:32 +0200

    wignercoeff

--- lenspyx/tests/wigners.py
+++ lenspyx/tests/wigners.py
@@ -9,7 +9,7 @@ from lenspyx.utils_hp import gauss_beam
 from lenspyx.wigners import wigners
 
 print('All these numbers must be small')
-lmaxs = [500, 1000, 2000, 4000, 5000]
+lmaxs = [500, 1000, 2000, 4000]
 for lmax in lmaxs:
     npts = (3 * lmax) // 2 + 1
     ls = np.arange(lmax + 1)
@@ -22,18 +22,27 @@ for lmax in lmaxs:
         for s2 in range(-3, 4):
             xi12 = wigners.wignerpos(cl_in, tht, s1, s2)
             clv2 = wigners_pl.wignercoeff(xi12 * wg, xg, s1, s2, lmax)
+            clv3 = wigners.wignercoeff(xi12 * wg, tht, s1, s2, lmax)
             t_ls = ls[max(abs(s1), abs(s2)):]
             maxdevs.append(np.max(np.abs(clv2[t_ls] / cl_in[t_ls] - 1.)))
+            maxdevs.append(np.max(np.abs(clv3[t_ls] / cl_in[t_ls] - 1.)))
     print('lmax %s %.3e'%(lmax, np.max(maxdevs)))
     assert np.max(maxdevs) < 1e-9, np.max(maxdevs)
-    if lmax == 2000:
+    if lmax == 4000:
         for s1s2 in [(0, 0), (0, 2), (2, 0), (2, 2), (0, -2), (-2, 0), (-2, -2), (1, 3)]:
             s1, s2 = s1s2
-            print('Timings s1 s2  %s %s'%(s1, s2))
             t0 = time.time()
             xi12 = wigners.wignerpos(cl_in, tht, s1, s2)
             dt1 = time.time() - t0
             t0 = time.time()
             xi12 = wigners_pl.wignerpos(cl_in, xg, s1, s2)
             dt2 = time.time() - t0
-            print('speed-up %.3f'%(dt2 / dt1))
+            print('%2s %2s fwd speed-up %.3f'%(s1, s2, dt2 / dt1))
+            s1, s2 = s1s2
+            t0 = time.time()
+            cl = wigners.wignercoeff(xi12, tht, s1, s2, lmax)
+            dt1 = time.time() - t0
+            t0 = time.time()
+            cl = wigners_pl.wignercoeff(xi12, xg, s1, s2, lmax)
+            dt2 = time.time() - t0
+            print('%2s %2s adj speed-up %.3f'%(s1, s2, dt2 / dt1))
--- lenspyx/wigners/wigners.py
+++ lenspyx/wigners/wigners.py
@@ -4,7 +4,7 @@
 """
 from __future__ import annotations
 import numpy as np
-from ducc0.sht.experimental import alm2leg
+from ducc0.sht.experimental import alm2leg, leg2alm
 
 
 def wignerpos(cl, theta, s1, s2):
@@ -45,3 +45,29 @@ def wignerpos(cl, theta, s1, s2):
         return (-1 if s % 2 == 0 else 1) * (leg[0].real - leg[1].imag)
 
     assert 0, (s1, s2)
+
+
+def wignercoeff(xi, tht, s1, s2, lmax):
+    if s1 < 0:
+        t_xi = xi if (s1 + s2) % 2 == 0 else -xi
+        return wignercoeff(t_xi, tht, -s1, -s2, lmax)
+    if s1 == 0 and s2 != 0:
+        # always want a spin 0 on the SHT side
+        t_xi = xi if (s1 + s2) % 2 == 0 else -xi
+        return wignercoeff(t_xi, tht, s2, s1, lmax)
+    mval = np.array([abs(s1)], dtype=int)
+    mstart = np.array([0], dtype=int)
+    fac = (2 * np.pi * np.sqrt(4 * np.pi)) / np.sqrt(np.arange(1, 2 * lmax + 3, 2))
+    xis = xi.astype(complex)
+    if s2 == 0:
+        cl = leg2alm(leg=np.reshape(xis, (1, xi.size, 1)), spin=0, mval=mval, mstart=mstart, theta=tht,
+                       lmax=lmax,  mode='STANDARD').squeeze().real
+        sgn = 1 if s1 > 0 else (1 if abs(s1) % 2 == 0 else -1)
+        return sgn * cl * fac
+    else:
+        xis = np.stack([xis, 1j * np.sign(s2) * xis]).reshape((2, xi.size, 1))
+        cl = leg2alm(leg=xis, spin=abs(s2), mval=mval, mstart=mstart, theta=tht,
+                       lmax=lmax,  mode='STANDARD').squeeze()[0].real
+        sgn = -1 if s2 > 0 else (-1 if abs(s2) % 2 == 0 else 1)
+        return sgn * cl * fac
+
