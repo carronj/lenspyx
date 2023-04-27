0e69c27b11e559eb29551e5b61e4b3cbd718e2a9
jcarron <to.jcarron@gmail.com>
2023-04-27 16:48:59 +0200

    wigner tests

--- lenspyx/tests/wigners/wigners.py
+++ lenspyx/tests/wigners/wigners.py
@@ -24,17 +24,22 @@ for lmax in lmaxs:
     for s1 in range(-3, 4):
         for s2 in range(-3, 4):
             xi12 = wigners.wignerpos(cl_in, tht, s1, s2)
+            xi12_2 = wigners.wigner4pos(cl_in, None, tht, s1, s2)[0 if s2 >= 0 else 1]
             clv2 = wigners_pl.wignercoeff(xi12 * wg, xg, s1, s2, lmax)
             clv3 = wigners.wignercoeff(xi12 * wg, tht, s1, s2, lmax)
+            clv4 = wigners.wignercoeff(xi12_2 * wg, tht, s1, s2, lmax)
             t_ls = ls[max(abs(s1), abs(s2)):]
             maxdevs.append(np.max(np.abs(clv2[t_ls] / cl_in[t_ls] - 1.)))
+            maxdevs.append(np.max(np.abs(clv4[t_ls] / cl_in[t_ls] - 1.)))
             maxdevs.append(np.max(np.abs(clv3[t_ls] / cl_in[t_ls] - 1.)))
+            assert np.max(maxdevs) < 1e-9, (s1, s2)
+
     print('lmax %s %.3e'%(lmax, np.max(maxdevs)))
     assert np.max(maxdevs) < 1e-9, np.max(maxdevs)
     if lmax == 4000:
         print('speed-ups')
-        for s1 in range(-3, 4):
-            for s2 in range(-3, 4):
+        for s1 in range(-2, 3):
+            for s2 in range(-2, 3):
                 t0 = time.time()
                 xi12 = wigners.wignerpos(cl_in, tht, s1, s2)
                 dt1 = time.time() - t0
@@ -49,3 +54,4 @@ for lmax in lmaxs:
                 cl = wigners_pl.wignercoeff(xi12, xg, s1, s2, lmax)
                 dt2 = time.time() - t0
                 print('%2s %2s adj %.3f'%(s1, s2, dt2 / dt1))
+
