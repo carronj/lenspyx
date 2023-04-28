66f7702321043a1415217b08ef2488044fd45128
jcarron <to.jcarron@gmail.com>
2023-04-28 09:37:48 +0200

    minors

--- lenspyx/wigners/wigners.py
+++ lenspyx/wigners/wigners.py
@@ -95,30 +95,11 @@ def wignerpos(cl: np.darray[float], theta: np.darray[float], s1: int, s2: int):
         Returns:
             real array of same size than theta
 
+        Note:
+            You can use *wigner4pos* instead if you also need the result for -s2 (e.g. for :math:`\xi_{\pm}`)
 
     """
-    if s1 == 0 and s2 != 0:
-        # always want a spin 0 on the SHT side
-        t_cl = cl if (s1 + s2) % 2 == 0 else -cl
-        return wignerpos(t_cl, theta, s2, s1)
-    if s1 < 0:
-        t_cl = cl if (s1 + s2) % 2 == 0 else -cl
-        return wignerpos(t_cl, theta, -s1, -s2)
-    if s1 >= 0:
-        lmax = len(cl) - 1
-        s = abs(s2)
-        mstart = np.array([0], dtype=int)
-        mval = np.array([abs(s1)], dtype=int)
-        glm_r = (cl * np.sqrt(np.arange(1, 2 * lmax + 3, 2)) / np.sqrt(4 * np.pi)).astype(complex)
-        mode = 'GRAD_ONLY' if s else 'STANDARD'
-        leg = alm2leg(alm=np.atleast_2d(glm_r), spin=s, lmax=lmax, mval=mval, mstart=mstart, theta=theta, mode=mode).squeeze()
-        if s2 == 0:
-            return leg.real
-        if s2 > 0:
-            return -(leg[0].real + leg[1].imag)
-        return (-1 if s % 2 == 0 else 1) * (leg[0].real - leg[1].imag)
-
-    assert 0, (s1, s2)
+    return wigner4pos(cl, None, theta, s1, s2)[0 if s2 >= 0 else 1]
 
 
 def wignercoeff(xi: np.ndarray[float], theta: np.ndarray[float], s1: int, s2: int, lmax: int):
